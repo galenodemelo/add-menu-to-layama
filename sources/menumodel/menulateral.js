0885789312141f0ea8a28f32aecd73973d68c50d
Galeno de Melo <galenodemelo@gmail.com>
2022-06-28 20:07:18 -0300

    fix: Opening menu

--- sources/menumodel/menulateral.js
+++ sources/menumodel/menulateral.js
@@ -5,13 +5,13 @@ var navegacaoLateral = document.getElementById("sidenav")
 
 function openNav() {
     if (!abriu) {
-        navegacaoLateral.style.margin = "0"
+        navegacaoLateral.classList.add("opened")
         imagemDoBotao.src = "./menu/img/button_02.gif"
 
         abriu = true
         clearInterval(intervaloAtualizaBotaoMais)
     } else {
-        navegacaoLateral.style.margin = "0 -320px 0 0"
+        navegacaoLateral.classList.remove("opened")
         imagemDoBotao.src = "./menu/img/button_01.gif"
 
         abriu = false
